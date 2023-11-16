efc6e7f192f32fc537fa8d22888f451b7a792070
Gregory Hernandez <ha.alexis20@gmail.com>
2023-11-16 12:58:38 -0300

    Agregar archivos para prueba de acción

---
+++ .github/workflows/hola_mundo.py
@@ -0,0 +1,13 @@
+import os
+
+def main():
+    # Obtener el secreto desde la variable de entorno
+    secret_message = os.getenv('GITHUB_SECRET_MESSAGE')
+
+    if secret_message:
+        print(f"Mensaje secreto desde GitHub: {secret_message}")
+    else:
+        print("No se pudo obtener el mensaje secreto. Verifica la configuración del secreto en GitHub.")
+
+if __name__ == "__main__":
+    main()
