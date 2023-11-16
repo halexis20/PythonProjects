2c1384b65884f2b14653a88bbd41e038d0df1065
Gregory Hernandez <ha.alexis20@gmail.com>
2023-11-16 13:06:09 -0300

    t

--- hola_mundo.py
+++ hola_mundo.py
@@ -2,7 +2,7 @@ import os
 
 def main():
     # Obtener el secreto desde la variable de entorno
-    secret_message = os.getenv('GITHUB_SECRET_MESSAGE')
+    secret_message = os.getenv('GS_SECRET_MESSAGE')
 
     if secret_message:
         print(f"Mensaje secreto desde GitHub: {secret_message}")
